4cb61903075429137f758e7b701f058a640e5ea6
Marcel Vlcek <vlcek.marc@gmail.com>
2024-11-18 21:16:38 +0100

    fetch and display dog breeds from API

--- src/components/DogBreeds.js
+++ src/components/DogBreeds.js
@@ -1,15 +1,40 @@
+import { useState, useEffect } from 'react'
+import '../css/DogBreed.css'
 
 const DogBreeds = () => {
-  const url = "https://api.kanye.rest/"
+  const url = "https://api.thedogapi.com/v1/breeds"
+  
+  const [dogBreeds, setDogBreeds] = useState([])  
+  
   const getBreed = async () => {
-  const response = await fetch(url)
-  const data = await response.json()
-  console.log(data["quote"])
+    const response = await fetch(url, {
+      headers: {
+        'x-api-key': 'live_SuYPHKLerg5vyOCBxpw08WTn8UWjmClTAyDquG1l3MV3uhuXMXcKAn51T4Un2Q0m'  
+      }
+    })
+    const data = await response.json()
+
+    const shuffledBreeds = data.sort(() => 0.5 - Math.random())
+    const randomBreeds = shuffledBreeds.slice(0, 10)
+
+  setDogBreeds(randomBreeds)
+
+    
   }
-  getBreed()
+
+  
+
+  useEffect(() => {
+    getBreed()  
+  }, [])  
 
   return (
-    <div>DogBreeds</div>
+    <div>
+      
+      {dogBreeds.map((breed, index) => (
+        <p className="dog-breed-item" key={index}>{breed.name}<button>show more</button></p>  
+      ))}
+    </div>
   )
 }
 

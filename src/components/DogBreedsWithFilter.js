ec9248c3b039487f1da9eedcdce71f01115b6065
Marcel Vlcek <vlcek.marc@gmail.com>
2024-11-19 10:44:35 +0100

    Add functional filter for dog breeds

--- src/components/DogBreedsWithFilter.js
+++ src/components/DogBreedsWithFilter.js
@@ -3,11 +3,10 @@ import '../css/DogBreedsWithFIlter.css'
 
 const DogBreedsWithFilter = () => {
 
-  const [searchingText, setSearchingText] = useState("")
-
   const url = "https://api.thedogapi.com/v1/breeds"
   
   const [dogBreeds, setDogBreeds] = useState([])  
+  const [searchTerm, setSearchTerm] = useState('')
   
   const getBreed = async () => {
     const response = await fetch(url, {
@@ -15,6 +14,7 @@ const DogBreedsWithFilter = () => {
         'x-api-key': 'live_SuYPHKLerg5vyOCBxpw08WTn8UWjmClTAyDquG1l3MV3uhuXMXcKAn51T4Un2Q0m'  
       }
     })
+    
     const data = await response.json()
 
     const shuffledBreeds = data.sort(() => 0.5 - Math.random())
@@ -25,23 +25,35 @@ const DogBreedsWithFilter = () => {
     
   }
 
-  
 
   useEffect(() => {
     getBreed()  
   }, [])  
 
+  const filteredBreeds = dogBreeds.filter((breed) => 
+    breed.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+  )
+
   return (
     <div>
-       <form>
-        <label htmlFor='searchInput'>Filter by breed name</label>
-        <input id='searchInput' type="text" placeholder='Breed name' onChange={
-          (e) => setSearchingText(e.target.value)
-        } />
-    </form>
-      {dogBreeds.map((breed, index) => (
-        <p className="dog-breed-item" key={index}><strong>{breed.name}</strong><button>show more</button></p>  
-      ))}
+      <form>
+        <label htmlFor="searchInput">Filter by breed name</label>
+        <input
+          id="searchInput"
+          type="text"
+          placeholder="Breed name"
+          onChange={(e) => setSearchTerm(e.target.value)} 
+        />
+      </form>
+
+      <div>
+        {filteredBreeds.map((breed, index) => (
+          <p className="dog-breed-item" key={index}>
+            <strong>{breed.name}</strong>
+            <button>show more</button>
+          </p>
+        ))}
+      </div>
     </div>
   )
 }
